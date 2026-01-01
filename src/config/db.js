3b82c003169c800b873be06f546a8620d117c6ed
Akshada Bhamare <akshadabhamare22@gmail.com>
2026-01-01 15:21:10 +0530

    Railway MySQL config

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,12 @@
 import mysql from "mysql2";
 
 export const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "yourpassword",
-  database: "bhamare_classes",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
